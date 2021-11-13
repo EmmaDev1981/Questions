49a3dd8d5b969734d62c874b2bcb04792cdc6d7e
Emmanuel <emanuelnavas@hotmail.com>
2021-11-13 13:39:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <Questions  />
+</template>
+
+<script>
+import Questions from './components/Questions.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Questions
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
